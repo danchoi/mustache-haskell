1bdd93a2c7a3411820cf003eb0d0afbf532472fd
Daniel Choi <dhchoi@gmail.com>
2014-12-08 13:03:14 -0500

    cleanup

--- Text/Mustache.hs
+++ Text/Mustache.hs
@@ -60,9 +60,6 @@ mergeValues _ inner = inner
 evalToBuilder :: KeyPath -> Value -> B.Builder
 evalToBuilder k v = valToBuilder $ evalKeyPath k v
 
-evalToText :: KeyPath -> Value -> Text
-evalToText k v = valToText $ evalKeyPath k v
-
 -- evaluates the a JS key path against a Value context to a leaf Value
 evalKeyPath :: KeyPath -> Value -> Value
 evalKeyPath [] x@(String _) = x
@@ -87,22 +84,12 @@ evalKeyPath _ _ = Null
 valToBuilder :: Value -> B.Builder
 valToBuilder (String x) = B.fromText x
 valToBuilder Null = B.fromText "null"
-valToBuilder (Bool True) = B.fromText "t"
-valToBuilder (Bool False) = B.fromText "f"
+valToBuilder (Bool True) = B.fromText "true"
+valToBuilder (Bool False) = B.fromText "false"
 valToBuilder (Number x) = 
     case floatingOrInteger x of
         Left float -> B.realFloat float
         Right int -> B.decimal int
 valToBuilder (Object x) = B.fromText . T.pack . show $ x
 
-valToText :: Value -> Text
-valToText (String x) = x
-valToText Null = "null"
-valToText (Bool True) = "t"
-valToText (Bool False) = "f"
-valToText (Number x) = 
-    case floatingOrInteger x of
-        Left float -> T.pack . show $ float
-        Right int -> T.pack . show $ int
-valToText (Object _) = "[Object]"
 
