a412de258377d1f74c117c0f9b1ba8da330863c1
Daniel Choi <dhchoi@gmail.com>
2014-12-08 16:54:29 -0500

    version

--- Main.hs
+++ Main.hs
@@ -22,7 +22,7 @@ opts = info
           (helper <*> parseOpts)
           (fullDesc 
             <> progDesc "A Haskell implementation of Mustache templates.\nOn STDIN provide the JSON to insert into the template."
-            <> header "mustashehs v0.1.4.1")
+            <> header "mustashehs v0.1.0.0")
 
 main = do
     opts' <- execParser opts 
